480467d3dbfbaf6db167e66817e49f51874bc7e0
Igor Tereshchenko <tereshchenko.igor@gmail.com>
2017-02-11 20:50:00 +0200

    CreateMarkers markers coordinates to string array

--- GoogleMapHandlers/CreateMarkersJS.m
+++ GoogleMapHandlers/CreateMarkersJS.m
@@ -1,10 +1,26 @@
 function CreateMarkersJS( coordinates )
 %   coordinates=[[lat,lng];[lat,lng];[lat,lng]]
 
+
+%var markers = [{lat: 37.769, lng: -122.446},{lat: 58.769, lng: -142.446}];
+markers='var markers = [';
 for i=1:length(coordinates)
-    markers=strcat(num2str(i),',')
+    
+    currect_coordinate=coordinates(i,:);
+    
+    marker=strcat('{lat: ',num2str(currect_coordinate(1)),', lng: ',num2str(currect_coordinate(2)),'}');
+    if (i==1)
+        markers=strcat(markers,marker);
+    else
+        markers=strcat(markers,',',marker);
+    end
 end
 
+markers=strcat(markers,'];')
+
+fid = fopen( 'markers.js', 'w' );
+fprintf( fid, '%s', markers);
+fclose(fid);
 
 end
 
